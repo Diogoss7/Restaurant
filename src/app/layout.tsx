60ea8cfaeaa2ad713ef02663a63aef1ea9ce1f0f
diogo-dev1 <diogosilvadev1@gmail.com>
2025-02-19 21:07:39 -0300

    teste2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "FSW Donalds",
-  description: "Bora finaasdasdlizar esse projeto lindo!",
+  description: "Bora finaasdasdlizaasdar esse projeto lindo!",
 };
 
 export default function RootLayout({
