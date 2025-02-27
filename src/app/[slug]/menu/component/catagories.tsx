0728950af3dfdc222439afa58cc5ec97776eb0cf
diogo-dev1 <diogosilvadev1@gmail.com>
2025-02-27 18:39:32 -0300

    order and cart

--- src/app/[slug]/menu/component/catagories.tsx
+++ src/app/[slug]/menu/component/catagories.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/formatCurrency";
 
+import CartSheet from "../[productId]/components/cartSheet";
 import { CartContext } from "../context/cart";
 import Products from "./products";
 
@@ -29,7 +30,12 @@ const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
 const handleCategoryClick = (category:MenuCategoriesWithProducts) =>{
     setSelectedCategory(category)
 }
-const{products}= useContext(CartContext)
+const{
+    products,
+    total,
+    toggleCart,
+    totalQuantity
+}= useContext(CartContext)
 
 const getCategoryButtonVariant =(category:MenuCategoriesWithProducts)=>{
          return selectedCategory.id === category.id ? "default":"secondary"
@@ -68,12 +74,14 @@ const getCategoryButtonVariant =(category:MenuCategoriesWithProducts)=>{
             <Products products={selectedCategory.products}
             />
             {products.length > 0 && ( 
-                <div className="fixed bottom-0 left-0 right-0 fle w-full items-center justify-between border-t bg-white">
+                <div className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-between border-t bg-white px-5 py-3">
                  <div className="">
                     <p className="text-xs text-muted-foreground">Total dos Pedidos</p>
-                    <p className="text-sm font-semibold">{formatCurrency(products.length)}</p>
-                    <span> / {products.length}</span>
+                    <p className="text-sm font-semibold">{formatCurrency(total)}
+                    <span className="text-xs font-normal text-muted-foreground"> /{totalQuantity} {totalQuantity > 1 ? "itens":"item"}</span></p>
                  </div>
+                  <Button onClick={toggleCart}>Ver sacola</Button>
+                  <CartSheet/>
                 </div>
             )}
         </div>
