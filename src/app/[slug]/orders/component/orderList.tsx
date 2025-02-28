26b5002ceec43674bfe52d3fd67ed801b6f5d14b
diogo-dev1 <diogosilvadev1@gmail.com>
2025-02-28 11:10:15 -0300

    add corrections

--- src/app/[slug]/orders/component/orderList.tsx
+++ src/app/[slug]/orders/component/orderList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { OrderStatus, Prisma } from "@prisma/client";
-import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import { ChevronLeftIcon, Divide, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -34,8 +34,8 @@ const getStatusLabel = (status: OrderStatus) => {
   if (status === "FINISHED") return "Finalizado";
   if (status === "IN_PREPARATION") return "Em preparo";
   if (status === "PENDING") return "Pendente";
-//   if (status === "PAYMENT_CONFIRMED") return "Pagamento confirmado";
-//   if (status === "PAYMENT_FAILED") return "Pagamento falhou";
+  //   if (status === "PAYMENT_CONFIRMED") return "Pagamento confirmado";
+  //   if (status === "PAYMENT_FAILED") return "Pagamento falhou";
   return "";
 };
 
@@ -56,41 +56,51 @@ const OrderList = ({ orders }: OrderListProps) => {
         <ScrollTextIcon />
         <h2 className="text-lg font-semibold">Meus Pedidos</h2>
       </div>
-      {orders.map((order) => (
-        <Card key={order.id}>
-          <CardContent className="space-y-4 p-5">
-            <div
-              className={`w-fit rounded-full px-2 py-1 text-xs font-semibold text-white ${([OrderStatus.FINISHED, OrderStatus.FINISHED] as OrderStatus[]).includes(order.status) ? "bg-green-500 text-white" : "bg-gray-200 text-gray-500"} `}
-            >
-              {getStatusLabel(order.status)}
-            </div>
-            <div className="flex items-center gap-2">
-              <div className="relative h-5 w-5">
-                <Image
-                  src={order.restaurant.avatarImageUrl}
-                  alt={order.restaurant.name}
-                  className="rounded-sm"
-                  fill
-                />
-              </div>
-              <p className="text-sm font-semibold">{order.restaurant.name}</p>
-            </div>
-            <Separator />
-            <div className="space-y-2">
-              {order.orderProducts.map((orderProduct) => (
-                <div key={orderProduct.id} className="flex items-center gap-2">
-                  <div className="flex h-5 w-5 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white">
-                    {orderProduct.quantity}
-                  </div>
-                  <p className="text-sm">{orderProduct.product.name}</p>
+      {orders.length === 0 ? (
+        <>
+          <h1> Seu carrinho está vazio </h1>
+        </>
+      ) : (
+        <>
+          {orders.map((order) => (
+            <Card key={order.id}>
+              <CardContent className="space-y-4 p-5">
+                <div
+                  className={`w-fit rounded-full px-2 py-1 text-xs font-semibold text-white ${([OrderStatus.FINISHED, OrderStatus.FINISHED] as OrderStatus[]).includes(order.status) ? "bg-green-500 text-white" : "bg-gray-200 text-gray-500"} `}
+                >
+                  {getStatusLabel(order.status)}
                 </div>
-              ))}
-            </div>
-            <Separator />
-            <p className="text-sm font-medium">{formatCurrency(order.total)}</p>
-          </CardContent>
-        </Card>
-      ))}
+                <div className="flex items-center gap-2">
+                  <div className="relative h-5 w-5">
+                    <Image
+                      src={order.restaurant.avatarImageUrl}
+                      alt={order.restaurant.name}
+                      className="rounded-sm"
+                      fill
+                    />
+                  </div>
+                  <p className="text-sm font-semibold">{order.restaurant.name}</p>
+                </div>
+                <Separator />
+                <div className="space-y-2">
+                  {order.orderProducts.map((orderProduct) => (
+                    <div key={orderProduct.id} className="flex items-center gap-2">
+                      <div className="flex h-5 w-5 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white">
+                        {orderProduct.quantity}
+                      </div>
+                      <p className="text-sm">{orderProduct.product.name}</p>
+                    </div>
+                  ))}
+                </div>
+                <Separator />
+                <p className="text-sm font-medium">{formatCurrency(order.total)}</p>
+              </CardContent>
+            </Card>
+          ))}
+        </>
+
+      )}
+
     </div>
   );
 };
